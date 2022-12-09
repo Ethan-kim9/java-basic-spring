c884a69b0bf0ad891299ae38215a04f54907cb1f
EthanKim <hey0507@gmail.com>
2022-12-09 12:31:03 +0900

    :tada: Begin : 리포 생성 및 공부시작 (12/9)
    원데이 원커밋, 꾸준히 공부할 수 있는 시스템 만들

---
+++ src/main/java/me/ethan/javabasicspring/JavaBasicSpringApplication.java
@@ -0,0 +1,13 @@
+package me.ethan.javabasicspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBasicSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBasicSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/ethan/javabasicspring/JavaBasicSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ethan.javabasicspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBasicSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
